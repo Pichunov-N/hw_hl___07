86dc0624ebb81997382d58c4b32b6214056ffe51
Pichunov-N <npichunov@gmail.com>
2022-02-23 23:25:46 +0200

    completed seventh homework

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict"
+
+
+//1
+const sumTwoSmallestNumbers = (...args) => {
+    if (args == 1) {
+        return console.error("...");
+    } else {
+        const sortedArray = args.sort((a, b) => a - b);
+        return sortedArray[0] + sortedArray[1];
+    }
+};
+
+
+console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77));
+console.log(sumTwoSmallestNumbers(1, 3, 2));
+console.log(sumTwoSmallestNumbers(1));
+
+
+//2
+function createCalculator(firstNum) {
+    let result = firstNum;
+    return {
+        sum: (a) => {
+            result += a
+            return result
+        },
+        mult: (a) => {
+            result *= a
+            return result
+        },
+        sub: (a) => {
+            result -= a
+            return result
+        },
+        div: (a) => {
+            result /= a
+            return result
+        },
+    }
+}
+
+const calc = createCalculator(10);
+
+console.log(calc.sum(5));
+console.log(calc.mult(10));
+console.log(calc.sub(40));
+console.log(calc.div(10));
